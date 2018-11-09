e1372937c0674a0528b46d098179a2760bebf0c0
NHibiki <i@yuuno.cc>
2018-11-09 16:40:43 -0500

    :wrench: Support Async function & Window register

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /* Main Entry of Library */
-
+import 'babel-polyfill';
 import p5Gif from './components/p5Gif.js';
 
+if (typeof window !== "undefined") window.p5Gif = p5Gif;
 module.exports = p5Gif;
